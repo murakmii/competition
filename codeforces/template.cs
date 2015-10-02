b302518a3004d9153284500160353a4e0535edfb
bonono <bonono.jp@gmail.com>
2015-10-02 20:28:05 +0900

    テンプレ追加

---
+++ codeforces/template.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class ClassName
+{
+   public static void Main(string[] argv)
+   {
+      /*
+         Console.ReadLine();
+         Console.ReadLine();
+
+         Console.WriteLine("Edit here");
+      */
+   }
+}
