7411306c16ab7ccc77f9854c5c57cdc17daadf8c
bonono <bonono.jp@gmail.com>
2016-02-12 22:46:44 +0900

    微修正

--- codeforces/template.cs
+++ codeforces/template.cs
@@ -2,10 +2,8 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-public class ClassName
-{
-   public static void Main(string[] argv)
-   {
+public class Codeforces {
+   public static void Main(string[] argv) {
       /*
          Console.ReadLine();
          Console.ReadLine();
